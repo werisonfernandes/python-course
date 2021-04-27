84c8771cd1f33193d21849a6ffe302b208b120b8
Werison Aparecido Fernandes <waparecf@everis.com>
2021-04-27 14:43:23 -0300

    Variables lessons

---
+++ variables/variables-p2.py
@@ -0,0 +1,7 @@
+nome=input("Digite um funcionário: ")
+empresa=input("Digite a instituição: ")
+qtde_funcionarios=int(input("Digite a qtde de funcionários: "))
+mediaMensalidade=float(input("Digite a média da mensalidade: "))
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
